a5ae3d71bc434eb8a3832727efca4ffa242915b4
pkong0713 <57347885+pkong0713@users.noreply.github.com>
2019-11-05 09:43:47 +0800

    Update CAPM.py

--- CAPM.py
+++ CAPM.py
@@ -7,37 +7,37 @@ Mean of [3-month] [US-Treasury] over the period is chosen as the risk-free rate
 Inputs: <timeframe>, <rf_panel>, <market>, <asset>"""
 
 #--------------------------------Part 1: Setting up the model----------------------------------------
-#Importing the necessary libraries
+# Importing the necessary libraries
 import quandl
 import datetime as dt
 import yfinance as yf
 import numpy as np
 
-#Setting the timeframe
+# Setting the timeframe
 start_date = dt.datetime(2015,1,1)
 end_date = dt.datetime(2018,1,1)
 
-#Downlaoding the US-treasury yield (Rf) from Quandl with the timeframe
+# Downlaoding the US-treasury yield (Rf) from Quandl with the timeframe
 risk_free_rates = quandl.get("USTREASURY/YIELD", authtoken="nr6pF_pcH1fiHoruHFRa", start_date = start_date, end_date = end_date)
 
-#Getting the 3-month rate, and setting the Rf benchmark(Chosen using [:,2])
+# Getting the 3-month rate, and setting the Rf benchmark(Chosen using [:,2])
 rf_df = risk_free_rates.iloc[:, 2]
 rf = round(rf_df.mean()/100,4)
 
-#Downloading S&P 500 data (Benchmark) from yfinance
+# Downloading S&P 500 data (Benchmark) from yfinance
 market = "^GSPC"
 benchmark_market = yf.Ticker(market)
 
-#Getting expected return on market for Benchmark (SPY)
+# Getting expected return on market for Benchmark (SPY)
 benchmark_price = benchmark_market.history(interval="1mo", start = start_date, end = end_date, auto_adjust=True)['Close']
 benchmark_price_pct = benchmark_price.pct_change()
 benchmark_price_pct = benchmark_price_pct.dropna()
 rm = benchmark_price[-1]/benchmark_price[0]-1
 
-#Getting risk for Benchmark (SPY)
+# Getting risk for Benchmark (SPY)
 std_market = np.std(benchmark_price)
 
-#Getting return for Apple stock, "AAPL"
+# Getting return for Apple stock, "AAPL"
 ticker = "AAPL"
 asset = yf.Ticker(ticker)
 return_on_asset = asset.history(interval="1mo", start = start_date, end = end_date, auto_adjust = True)['Close']
@@ -46,21 +46,21 @@ return_on_asset_pct = return_on_asset.pct_change()
 return_on_asset_pct = return_on_asset_pct.dropna()
 ra = return_on_asset[-1]/return_on_asset[0]-1
 
-#Getting risk for Asset
+# Getting risk for Asset
 std_asset = np.std(return_on_asset)
 
-#Getting the Market Risk Premium & Variance of Benchmark
+# Getting the Market Risk Premium & Variance of Benchmark
 maket_risk_premium = rm - rf
 var_market = np.var(benchmark_price)
 
-#Getting the covariance and computing Beta_A
+# Getting the covariance and computing Beta_A
 cov_ab = np.cov(return_on_asset_pct,benchmark_price_pct)
 beta_a = round(cov_ab[0][1]/cov_ab[1][1],4)
 
-#Alpha based on the mean values
+# Alpha based on the mean values
 alpha = round(np.mean(return_on_asset_pct)-beta_a*np.mean(benchmark_price_pct),4)
 
-#Finally the CAPM
+# Finally the CAPM
 expected_return_on_asset = rf + beta_a * (maket_risk_premium)
 
 print(f'For the ticker {ticker} vs benchmark {market},'.format())
