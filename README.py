73e61574362fc05809dcbd3e07f0be271386d9b8
pkong0713 <57347885+pkong0713@users.noreply.github.com>
2019-11-05 09:42:24 +0800

    Create README.py

---
+++ README.py
@@ -0,0 +1,6 @@
+# Tools-for-Financial-Modelling
+
+# This repository is made for the collection of financial models which are commonly used.
+# It aims to deliver various range of tools used in financial modelling and quantitative finance,
+# including asset pricing models, portfolio management methods, and more.
+# Each file comes with a short description of the function of the model, as well as their parameters and inputs.
